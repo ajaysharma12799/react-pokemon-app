00fd3882cb358db6dad6022a294b42dc49c0ae94
Ajay Sharma <ajaysharma12799@gmail.com>
2021-10-13 22:54:41 +0530

    un-necessary changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="navbar navbar-expand-lg bg-dark navbar-dark text-white mt-3">
             <div className="container">
                 <h4 className="display-5">Pokemon</h4>
-                <p className="lead">Made with <span className="heart">&#10084;</span> by Tauseef</p>
+                <p className="lead">Made with <span className="heart">&#10084;</span> by Ajay</p>
             </div>
         </div>
     )
